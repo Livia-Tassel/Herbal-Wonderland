266154cbbb563c59cc7bf27d75adf33c9cbb4faa
Tassel <138864359+Livia-Tassel@users.noreply.github.com>
2025-09-19 23:30:24 +0800

    Revert "Refactor: Improve code structure and enhance UI/UX"

--- resources/code/js/home.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('startButton').addEventListener('click', function () {
-    this.style.display = 'none';
-    var blocks = document.getElementById('colorBlocks');
-    blocks.style.visibility = 'visible';
-    blocks.style.opacity = '1';
-});
